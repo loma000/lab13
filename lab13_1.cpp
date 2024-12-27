e099a40f55b2716537fdfe5da963749b093ecfdf
loma000 <dechartorn.som@gmail.com>
2024-12-28 01:22:00 +0700

    loma

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
@@ -18,4 +19,63 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int C, double B[])
+{
+
+    double sum1 = 0;
+    double sum2 = 1;
+    double max, min;
+
+    for (int i = 0; i < C; i++)
+    {
+        sum1 += A[i];
+    }
+    B[0] = sum1 / C;
+    sum1 = 0;
+    for (int j = 0; j < C; j++)
+    {
+        sum1 += pow(A[j] , 2);
+    }
+
+    B[1] = sqrt(( sum1  /  C  ) -pow(B[0],2));
+    sum1 = 0;
+    for (int j = 0; j < C; j++)
+    {
+        sum2 *= A[j];
+    }
+    B[2] = pow(sum2,1.0/C);
+
+    for (int j = 0; j < C; j++)
+    {
+        sum1 += 1 / A[j];
+    }
+
+    B[3] = C / sum1;
+    sum1 = 0;
+    for (int j = 0; j < C; j++)
+    {
+        if (j < 1)
+        {
+            max = A[j];
+            min = A[j];
+        }
+
+        else
+        {
+            if (max < A[j])
+            {
+                max = A[j];
+            }
+
+            if (min > A[j])
+            {
+                min = A[j];
+            }
+        }
+    }
+
+    B[4] = max;
+    B[5] = min;
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+ void updateImage(bool [][M],int,int,int);
 
 void showImage(const bool [][M]);
 
@@ -21,3 +21,56 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool loma[][M]){
+cout << "------------------------------------------------------------------------"<<endl;
+for (int i = 0; i <N ; i++)
+{ cout<<"|";
+    for (int j = 0; j < M ; j++)
+    {
+        
+        { if ( loma[i][j]==1)
+        {
+           cout << "*";
+        }else  
+        {
+             cout << " ";
+        }
+        
+        
+        
+           
+        }
+        
+        
+        
+    }
+     cout<<"|"<<endl;
+}
+cout << "------------------------------------------------------------------------" <<endl;
+
+
+
+
+
+
+};
+
+void updateImage(bool loma[][M],int a,int b ,int c){
+ 
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j  <  M ; j++)
+    {
+
+if (sqrt(pow(i-b,2)+pow(j-c,2))<=a-1)
+{
+    loma[i ][j ]=1;
+}
+
+
+
+
+
+
+    }}}
